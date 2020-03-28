f67d96a437f429a0fa52e640032e551261a4b556
kacper-marzecki <60017757+kacper-marzecki@users.noreply.github.com>
2020-03-28 18:29:24 +0100

    added frontend release files to tracked files

---
+++ src/main/resources/build/precache-manifest.01533941eb1657e116b2549dd49d05df.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40abdff820bccc99c6153d9d95770d39",
+    "url": "/add-conversation-info.png"
+  },
+  {
+    "revision": "3673a8157a8242e39319a9e08c1771ef",
+    "url": "/conversation-info.png"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "6ff2b8790ea30b6328508b88e72e1e4d",
+    "url": "/friends-info.png"
+  },
+  {
+    "revision": "c4dddb089f537bb74f9b4e9abf1b4685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "2b577457d39ec4f63448d53243b7cf98",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ea8c8398d5efdc2fd53d5fae5dc596f7",
+    "url": "/navbar-info.png"
+  },
+  {
+    "revision": "6e10627c38977fc1e5f52aef932a6f5a",
+    "url": "/poland.svg"
+  },
+  {
+    "revision": "ed0a7c2bb52193100c27",
+    "url": "/static/css/main.fb1d0a18.chunk.css"
+  },
+  {
+    "revision": "116cb6358730a7388cdf",
+    "url": "/static/css/vendors~main.1e2c9bd7.chunk.css"
+  },
+  {
+    "revision": "ed0a7c2bb52193100c27",
+    "url": "/static/js/main.ed0a7c2b.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "116cb6358730a7388cdf",
+    "url": "/static/js/vendors~main.116cb635.chunk.js"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/uk.svg"
+  }
+]);
